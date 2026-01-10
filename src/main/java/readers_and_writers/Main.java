2e687a429eefa0b27a032ebd6f38edfe069d29f1
w3rr0 <konradmateja65@gmail.com>
2026-01-11 00:12:18 +0100

    implement main class

--- src/main/java/readers_and_writers/Main.java
+++ src/main/java/readers_and_writers/Main.java
@@ -1,4 +1,31 @@
 package readers_and_writers;
 
 public class Main {
+    int readersCount = 10;
+    int writersCount = 3;
+
+    if (args.length >= 2) {
+        try {
+            readersCount = Integer.parseInt(args[0]);
+            writersCount = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Args should be integer");
+            return;
+        }
+    } else {
+        System.out.println("Too few arguments. Running with default args:");
+        System.out.println(readersCount + " readers, " + writetsCount + " writers");
+    }
+
+    Library library = new Library();
+
+    for (int i = 1; i <= writersCount; i++) {
+        Thread writerThread = new Thread(new Writer(library), "Writer-" + i);
+        writerThread.start();
+    }
+
+    for (int i = 1; i<= writersCount; i++) {
+        Thread readerThread = new Thread(new Reader(library), "Reader-" + i);
+        readerThread.start();
+    }
 }
