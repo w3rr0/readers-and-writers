b34b510bec8598c2b5d95685897448bb4c5a0422
w3rr0 <konradmateja65@gmail.com>
2026-01-11 00:17:39 +0100

    add entry point in main class

--- src/main/java/readers_and_writers/Main.java
+++ src/main/java/readers_and_writers/Main.java
@@ -4,28 +4,30 @@ public class Main {
     int readersCount = 10;
     int writersCount = 3;
 
-    if (args.length >= 2) {
-        try {
-            readersCount = Integer.parseInt(args[0]);
-            writersCount = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            System.out.println("Args should be integer");
-            return;
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            try {
+                readersCount = Integer.parseInt(args[0]);
+                writersCount = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Args should be integer");
+                return;
+            }
+        } else {
+            System.out.println("Too few arguments. Running with default args:");
+            System.out.println(readersCount + " readers, " + writetsCount + " writers");
         }
-    } else {
-        System.out.println("Too few arguments. Running with default args:");
-        System.out.println(readersCount + " readers, " + writetsCount + " writers");
-    }
 
-    Library library = new Library();
+        Library library = new Library();
 
-    for (int i = 1; i <= writersCount; i++) {
-        Thread writerThread = new Thread(new Writer(library), "Writer-" + i);
-        writerThread.start();
-    }
+        for (int i = 1; i <= writersCount; i++) {
+            Thread writerThread = new Thread(new Writer(library), "Writer-" + i);
+            writerThread.start();
+        }
 
-    for (int i = 1; i<= writersCount; i++) {
-        Thread readerThread = new Thread(new Reader(library), "Reader-" + i);
-        readerThread.start();
+        for (int i = 1; i<= writersCount; i++) {
+            Thread readerThread = new Thread(new Reader(library), "Reader-" + i);
+            readerThread.start();
+        }
     }
 }
