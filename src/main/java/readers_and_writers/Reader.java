d5fbc24aaf24d4e02b661d55812c4adbb37d0dba
w3rr0 <konradmateja65@gmail.com>
2026-01-10 22:45:36 +0100

    implement reader class

--- src/main/java/readers_and_writers/Reader.java
+++ src/main/java/readers_and_writers/Reader.java
@@ -1,4 +1,31 @@
 package readers_and_writers;
 
-public class Reader {
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Reader implements Runnable {
+    private final Library library;
+
+    public Reader(Library library) {
+        this.library = library
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                library.startReading();
+
+                int readingTime = ThreadLocalRandom.current().nextInt(1000, 3001);
+                Thread.sleep(readingTime);
+
+
+                library.stopReading();
+
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Thread (Reader) was interrupt");
+        }
+    }
 }
