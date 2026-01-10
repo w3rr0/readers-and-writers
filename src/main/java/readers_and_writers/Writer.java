2af7bf1b568995ebe7b285fd0fb94539401fdd52
w3rr0 <konradmateja65@gmail.com>
2026-01-11 00:01:18 +0100

    imlement writer

--- src/main/java/readers_and_writers/Writer.java
+++ src/main/java/readers_and_writers/Writer.java
@@ -1,4 +1,30 @@
 package readers_and_writers;
 
-public class Writer {
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Writer implements Runnabl e{
+    private final Library library;
+
+    public Writer(Library library) {
+        this.library = library;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                library.startWriting();
+
+                int writingTime = ThreadLocalRandom.current().nextInt(1000, 3001);
+                Thread.sleep(writingTime);
+
+                library.stopWriting();
+
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Thread (Writer) was interrupt");
+        }
+    }
 }
