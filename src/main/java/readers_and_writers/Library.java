815d3e3ad19c66c85297ad36571be3c739cfdd54
w3rr0 <konradmateja65@gmail.com>
2026-01-10 19:36:05 +0100

    add class fields in library

--- src/main/java/readers_and_writers/Library.java
+++ src/main/java/readers_and_writers/Library.java
@@ -1,4 +1,20 @@
 package readers_and_writers;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Library {
+    private final Lock lock = new ReentrantLock(true);
+    private final Condition condition = lock.newCondition();
+
+    private final LinkedList<Thread> waitQueue = new LinkedList<>();
+
+    private final List<Thread> activeReaders = new ArrayList<>();
+    private Thread activeWriter = null;
+
+    private static final int MAX_READERS = 5;
 }
