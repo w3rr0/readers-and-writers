f306f7c01e0dcef4032ba1e9bba72480bf99d63b
w3rr0 <konradmateja65@gmail.com>
2026-01-11 13:25:37 +0100

    test full loop with increased timeout

--- src/test/java/readers_and_writers/ThreadLoopTest.java
+++ src/test/java/readers_and_writers/ThreadLoopTest.java
@@ -8,25 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 class ThreadLoopTest {
 
     @Test
-    @Timeout(2)
+    @Timeout(10)
     void testReaderLoopAndInterrupt() throws InterruptedException {
         Library lib = new Library();
         Reader readerTask = new Reader(lib);
         Thread t = new Thread(readerTask);
 
         t.start();
-        Thread.sleep(100); 
+        Thread.sleep(5100); 
+
+        t.interrupt();
+        t.join();
     }
 
     @Test
-    @Timeout(2)
+    @Timeout(10)
     void testWriterLoopAndInterrupt() throws InterruptedException {
         Library lib = new Library();
         Writer writerTask = new Writer(lib);
         Thread t = new Thread(writerTask);
 
         t.start();
-        Thread.sleep(100); 
+        Thread.sleep(6100); 
 
         t.interrupt();
         t.join();
