835456680dd67642f6af7f84aed54f43c3e843f8
w3rr0 <konradmateja65@gmail.com>
2026-01-11 12:22:10 +0100

    add main test

---
+++ src/test/java/readers_and_writers/MainTest.java
@@ -0,0 +1,36 @@
+package readers_and_writers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testMainWithNoArguments() {
+        
+        String[] args = {"tekst", "zły"};
+        Main.main(args);
+        
+        String output = outContent.toString();
+        assertTrue(output.contains("Args should be integer"));
+    }
+}
