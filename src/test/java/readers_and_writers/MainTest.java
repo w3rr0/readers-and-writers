2b7cf84eb89fd44daea14f93e4c4469ed35b0c0b
w3rr0 <konradmateja65@gmail.com>
2026-01-11 13:41:39 +0100

    add args tests in main test

--- src/test/java/readers_and_writers/MainTest.java
+++ src/test/java/readers_and_writers/MainTest.java
@@ -3,6 +3,7 @@ package readers_and_writers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -27,10 +28,31 @@ class MainTest {
     @Test
     void testMainWithNoArguments() {
         
-        String[] args = {"tekst", "zły"};
+        String[] args = {"0", "0"};
+        Main.main(args);
+    }
+
+    @Test
+    void testMainWithInvalidArgs() {
+        String[] args = {"string1", "string2"};
         Main.main(args);
         
         String output = outContent.toString();
         assertTrue(output.contains("Args should be integer"));
+    }
+
+    @Test
+    @Timeout(5)
+    void testMainWithOneWriter() throws InterruptedException {
+        Thread mainThread = new Thread(() -> {
+            Main.main(new String[]{"0", "1"});
+        });
+
+        mainThread.start();
+        Thread.sleep(500); 
+
+        mainThread.interrupt();
+        
+        mainThread.join();
     }
 }
